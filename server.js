35fbef81d896dd715a04d4ff9492450b037f16c8
mastas3 <rpsm90@gmail.com>
2018-10-04 11:25:03 +0300

    fix typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const path = requrie('path');
+const path = require('path');
 const bodyParser = require('body-parser');
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
